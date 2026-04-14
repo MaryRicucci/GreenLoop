3bf1bae7afd97cd3d6f59b12caab92396915cfb8
MaryRicucci <maria.ricucci.12@gmail.com>
2026-04-14 21:27:17 +0200

    Assegnazione punti Utente

--- backend/php/completaMissione.php
+++ backend/php/completaMissione.php
@@ -11,7 +11,7 @@ $idMissione = $_POST["id_Missione"];
 $id = $_SESSION["id_Utente"];
 
 //Controllo il file
-if (!isset($_FILE("foto"))){
+if (!isset($_FILES("foto"))){
     json_encode([
         "success" => false ,
         "message" => "Nessun file trovato" 
@@ -22,7 +22,7 @@ if (!isset($_FILE("foto"))){
 $foto = $_FILES["foto"];
 //Validazione tipo file
 $allowed = ["image/jpeg","image/png"];
-if (!in_array($foto["type"]),$allowed){
+if (!in_array($foto["type"],$allowed)){
     echo json_encode([
         "success" => false ,
         "message" => "Formato non valido. Usa JPG o PNG"
@@ -34,7 +34,7 @@ $host = "localhost";
 $pass = "";
 $user = "root";
 $conn =  new mysqli($host,$user,$pass,$db);
-if ($conn -> connect_erro){
+if ($conn -> connect_error){
     die("Connessione fallita");
 }
 $query = "SELECT * from Missioni_completate WHERE id_Utente = ? AND id_Missione = ? AND stato = 'approved' ";
@@ -54,10 +54,10 @@ $uploadDir =__DIR__. "/uploads/missioni/";
 if (!file_exists($uploadDir)){
     mkdir($uploadDir,0777,true);
 }
-$ext = pathinfo($foto,["name"],PATHINFO_EXTENSION());
-$filename = "missione_".$idUtente."_".time()."".$ext ;
+$ext = pathinfo($foto["name"],PATHINFO_EXTENSION());
+$filename = "missione_".$id."_".time()."".$ext ;
 $filepath = $uploadDir.$filename;
-if(!move_uploaded_file($foto["tmp-name"],$filepath)){
+if(!move_uploaded_file($foto["tmp_name"],$filepath)){
     echo json_encode([
         "success" => false ,
         "message" => "Errore nel salvataggio file"
@@ -65,11 +65,32 @@ if(!move_uploaded_file($foto["tmp-name"],$filepath)){
     exit ;
 }
 //Percorso da salvare nel DB
-$fotoDB = "/uplads/missioni/".$filename;
+$fotoDB = "/uploads/missioni/".$filename;
 
-$query = "SELECT punti from Missione WHERE id_Missione = ?";
+$query = "SELECT punti from Missioni WHERE id_Missione = ?";
 //Prepare
 //Assegna punti
-
-
+$template = $conn -> prepare($query) ;
+$template -> bind_param("i",$idMissione);
+$template -> execute();
+$punti = $template -> get_result();
+$punti = $punti -> fetch_assoc();
+if (!isset($punti)){
+    echo json_encode([
+        "success" => false ,
+        "message" => "Non è stato possibile trovare i punti Missione"
+    ]);
+    exit ;
+}
+$query = "UPDATE Utenti set punti = ? WHERE id_Utente = ?";
+$template = $conn -> prepare($query);
+$template -> bind_param("ii",$punti["punti"],$id);
+if($template -> execute()){
+    echo json_encode([
+        "success" : true,
+        "message" : "Prove della missione caricate con successo",
+        "punti" : $punti
+    ]);
+}
+$conn -> close();
 ?>
