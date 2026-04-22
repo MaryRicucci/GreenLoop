d177c1421c1ebb5936adbb6799d5caf3af10b4f0
MaryRicucci <maria.ricucci.12@gmail.com>
2026-04-22 13:38:52 +0200

    Inizio del frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+//File principale dell'app
+//Da modificare
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Login from "./pages/Login";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
